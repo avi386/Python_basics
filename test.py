22b4c4192f9ea5313f4d419ef309bd62a3555360
Aishwarya1226 <aishwaryars1226@gmail.com>
2026-06-25 16:53:21 +0530

    python basics

---
+++ test.py
@@ -0,0 +1,23 @@
+print("Hello World","world",sep="#")
+#print("Hello World")
+#print("Hello World")
+x="10.5"
+y="s"
+# print("abc">"abd")
+# x=int(input())
+# X=int(input(float(x)))
+# print("\test\ntesting")
+# print(r"\test\ntesting")
+# print("\\test\ntesting")
+# print("""hello
+#       my name is 
+#      bala
+#      """)
+# x="hello"
+# print(x[0:3:2]) # start:end:step
+# print(x[::-1]) #reverse string
+for i in range (0,10,2):
+    if i%2==0:
+        print(i)
+#  while True:
+#   print("hello world")
